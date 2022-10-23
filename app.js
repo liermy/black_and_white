a8ee8652a767b450002832d58919ff45215e2525
Guilherme Dezorzi <112406712+liermy@users.noreply.github.com>
2022-10-23 15:45:25 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+var trocar = document.getElementById('troca');
+var btn = document.getElementById('btn')
+var element = document.body;
+var titulo = document.getElementById('title')
+
+btn.onclick = function() {
+    if(btn.value === 'white'){
+        trocar.src = './img/sol.png';
+        btn.value = 'black';
+        btn.innerHTML = 'WhiteMode'
+        element.classList.toggle("dark-mode");
+        titulo.innerHTML = 'WhiteMode using JavaScript'
+}else {
+    trocar.src = './img/lua.png';
+    btn.value = 'white';
+    btn.innerHTML = 'BlackMode';
+    element.classList.toggle("dark-mode")
+    titulo.innerHTML = 'BlackMode using JavaScript'
+}}
