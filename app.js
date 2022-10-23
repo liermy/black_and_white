106141544f8e737b2d324805ee26bb1fe83d5748
Guilherme Dezorzi <112406712+liermy@users.noreply.github.com>
2022-10-23 15:38:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var titulo = document.getElementById('title')
 
 btn.onclick = function() {
     if(btn.value === 'white'){
-        trocar.src = './img_/sol.png';
+        trocar.src = './img/sol.png';
         btn.value = 'black';
         btn.innerHTML = 'WhiteMode'
         element.classList.toggle("dark-mode");
         titulo.innerHTML = 'WhiteMode using JavaScript'
 }else {
-    trocar.src = './img_/lua.png';
+    trocar.src = './img/lua.png';
     btn.value = 'white';
     btn.innerHTML = 'BlackMode';
     element.classList.toggle("dark-mode")
